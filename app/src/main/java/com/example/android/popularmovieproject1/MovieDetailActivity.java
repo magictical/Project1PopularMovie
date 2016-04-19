6b8166b36eccecf48679c45eace10f097a2d1845
Ian <magictical@gmail.com>
2016-04-19 10:07:16 +0900

    add setting menu, DetailActivity with fragment (ignore the previous commit)

---
+++ app/src/main/java/com/example/android/popularmovieproject1/MovieDetailActivity.java
@@ -0,0 +1,18 @@
+package com.example.android.popularmovieproject1;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MovieDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_detail);
+    }
+
+}
